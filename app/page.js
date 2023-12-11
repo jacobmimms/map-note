ecafbb1f58c03ae6372fac755762e6b9784434f8
Jacob Mimms <jacobmimms@gmail.com>
2023-12-11 14:53:52 -0500

    plz work

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client';
+
 import { useState, useEffect } from 'react';
 import Upload from './components/upload';
 import dynamic from 'next/dynamic';
@@ -7,7 +8,7 @@ import Loading from './components/loading';
 const useUserPosition = () => {
   const [position, setPosition] = useState(null);
   const [error, setError] = useState(null);
-
+  const windowExists = typeof window !== 'undefined';
   const onSuccess = ({ coords }) => {
     const { latitude, longitude } = coords;
     setPosition([latitude, longitude]);
@@ -20,28 +21,18 @@ const useUserPosition = () => {
   useEffect(() => {
     let isMounted = true;
     let watchId;
-
-    const getUserLocation = () => {
-      if (isMounted && 'geolocation' in navigator) {
-        const geo = navigator.geolocation;
-        watchId = geo.watchPosition(onSuccess, onError);
-      } else {
-        setError('Geolocation is not supported');
-      }
-    };
-
-    const checkNavigator = () => {
-      if (window && 'navigator' in window) {
-        clearInterval(checkInterval);
-        getUserLocation();
-      }
-    };
-
-    let checkInterval = setInterval(checkNavigator, 1000); // Check every second
+    if (!windowExists) {
+      return;
+    }
+    if ('geolocation' in navigator) {
+      const geo = navigator.geolocation;
+      watchId = geo.watchPosition(onSuccess, onError);
+    } else {
+      setError('Geolocation is not supported');
+    }
 
     return () => {
       isMounted = false;
-      clearInterval(checkInterval);
       if (watchId) {
         navigator.geolocation.clearWatch(watchId);
       }
@@ -66,6 +57,7 @@ const Page = () => {
           <DynamicMap position={position} /> :
           <div className='flex items-center justify-center w-full h-full bg-slate-500'>
             <Loading />
+
           </div>
         }
       </main>
