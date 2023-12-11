c842f395d4decbd5c6fe39e3efc02f2842c070cb
Jacob Mimms <jacobmimms@gmail.com>
2023-12-11 15:09:33 -0500

    use other persons hook

--- app/page.js
+++ app/page.js
@@ -3,46 +3,43 @@ import { useState, useEffect } from 'react';
 import Upload from './components/upload';
 import dynamic from 'next/dynamic';
 import Loading from './components/loading';
+import useGeolocation from "react-hook-geolocation";
 
-const useUserPosition = () => {
-  const [position, setPosition] = useState(null);
-  const [error, setError] = useState(null);
+// const useUserPosition = () => {
+//   const [position, setPosition] = useState(null);
+//   const [error, setError] = useState(null);
 
 
 
-  const onSuccess = ({ coords }) => {
-    const { latitude, longitude } = coords;
-    setPosition([latitude, longitude]);
-  };
+//   const onSuccess = ({ coords }) => {
+//     const { latitude, longitude } = coords;
+//     setPosition([latitude, longitude]);
+//   };
 
-  const onError = (err) => {
-    setError(err.message);
-  };
+//   const onError = (err) => {
+//     setError(err.message);
+//   };
 
-  useEffect(() => {
-    if (typeof navigator === 'undefined') {
-      return;
-    }
+//   useEffect(() => {
+//     let isMounted = true;
+//     let watchId;
+//     if ('geolocation' in navigator) {
+//       const geo = navigator.geolocation;
+//       watchId = geo.watchPosition(onSuccess, onError);
+//     } else {
+//       setError('Geolocation is not supported');
+//     }
 
-    let isMounted = true;
-    let watchId;
-    if ('geolocation' in navigator) {
-      const geo = navigator.geolocation;
-      watchId = geo.watchPosition(onSuccess, onError);
-    } else {
-      setError('Geolocation is not supported');
-    }
+//     return () => {
+//       isMounted = false;
+//       if (watchId) {
+//         navigator.geolocation.clearWatch(watchId);
+//       }
+//     };
+//   }, []);
 
-    return () => {
-      isMounted = false;
-      if (watchId) {
-        navigator.geolocation.clearWatch(watchId);
-      }
-    };
-  }, []);
-
-  return { position, error };
-};
+//   return { position, error };
+// };
 
 
 const DynamicMap = dynamic(() => import('./components/map'), {
@@ -50,7 +47,15 @@ const DynamicMap = dynamic(() => import('./components/map'), {
 });
 
 const Page = () => {
-  const { position, error } = useUserPosition();
+  const [position, setPosition] = useState(null);
+  const geolocation = useGeolocation();
+
+
+  useEffect(() => {
+    if (geolocation.latitude && geolocation.longitude) {
+      setPosition([geolocation.latitude, geolocation.longitude]);
+    }
+  }, [geolocation])
 
   return (
     <>
