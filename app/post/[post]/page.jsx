62ed5a478376ea02fc9c82b2c1075f646fd7d4bb
Jacob Mimms <jacobmimms@gmail.com>
2023-12-21 20:37:04 -0500

    fixed dynamic server pages?

--- app/post/[post]/page.jsx
+++ app/post/[post]/page.jsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import { sql } from '@vercel/postgres';
 import { encodeS3Key } from '@/app/utils/main';
 import prisma from '@/lib/prisma';
 
@@ -14,23 +13,27 @@ export async function generateStaticParams() {
     const posts = await prisma.post.findMany()
     return posts.map((post) => ({
         params: {
-            post: post.title
+            post: post.title,
         }
     }))
+}
+
+async function getPostData(post) {
+    const postData = await prisma.post.findMany({
+        where: {
+            title: post
+        }
+    })
+    return postData[0].content;
+
 }
 
 const url = 'https://mimms-pictures.s3.amazonaws.com/'
 
 export default async function Page({ params }) {
     const { post } = params;
-    // const { rows } = await sql`SELECT * from post_db WHERE id = ${post}`;
-    const rows = await prisma.post.findMany({
-        where: {
-            title: post
-        }
-    })
-    const content = rows[0].content;
     let srcUrl = `${url}${encodeS3Key(post)}`
+    const content = await getPostData(post)
     return (
         <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white">
             <div className="bg-gray-700 p-4 rounded shadow-lg">
