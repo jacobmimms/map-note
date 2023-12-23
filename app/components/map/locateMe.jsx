c703c309e633c3f2c01f0bf5f87d4a286a20acb1
Jacob Mimms <jacobmimms@gmail.com>
2023-12-23 13:40:58 -0500

    kinda working, need to fix loading screen again

--- app/components/map/locateMe.jsx
+++ app/components/map/locateMe.jsx
@@ -22,7 +22,7 @@ export default function LocateMe() {
     const disabled = disable ? 'spinner' : 'text-white';
 
     return (
-        <div className='fixed bottom-4 left-4'>
+        <div className='fixed bottom-4 left-4 z-10'>
             <button disabled={disable} onClick={locateUser} className={`bg-slate-700 ${disabled} rounded-full p-2 w-12 h-12 flex items-center justify-center`}>
                 <FontAwesomeIcon icon={faLocation} className='w-6 h-6' />
             </button>
--- app/components/map/map.jsx
+++ app/components/map/map.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import Loading from '../animations/loading'
@@ -11,32 +11,33 @@ import LocateMe from './locateMe'
 function Map() {
     const mapRef = useRef(null);
     const savedLocation = localStorage.getItem('lastLocation');
-    const positionRef = useRef(
+    const [position, setPosition] = useState(
         savedLocation ? JSON.parse(savedLocation) : { latitude: 0, longitude: 0 }
     );
 
-    if (positionRef.current.latitude === 0 && positionRef.current.longitude === 0) {
-        return <div className={`flex items-center justify-center w-full h-full bg-slate-600`}><Loading /></div>;
-    }
+    // if (position.latitude === 0 && position.longitude === 0) {
+    //     return <div className={`flex items-center justify-center w-full h-full bg-slate-600`}><Loading /></div>;
+    // }
 
     const handleReady = (e) => {
+        console.log('map ready');
         mapRef.current = e.target;
         e.target.locate(
             {
                 setView: true,
                 maxZoom: 14,
-                watch: true,
                 enableHighAccuracy: true
             }
         );
         e.target.on('locationfound', (e) => {
+            setPosition({ latitude: e.latitude, longitude: e.longitude });
             localStorage.setItem('lastLocation', JSON.stringify({ latitude: e.latitude, longitude: e.longitude }));
         });
     }
 
 
     return (
-        <MapContainer className={`h-full w-full`} center={[positionRef.current.latitude, positionRef.current.longitude]} zoom={14} zoomControl={false} scrollWheelZoom={false} tap={false} whenReady={handleReady}>
+        <MapContainer className={`h-full w-full`} center={[position.latitude, position.longitude]} zoom={14} zoomControl={false} scrollWheelZoom={false} tap={false} whenReady={handleReady}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
